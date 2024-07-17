aec80545791f1b7a8d0549d9d06185431360dad2
Bayu Budianto <Bayu.Budianto@bni.co.id>
2024-07-17 10:59:35 +0700

    Add project files.

---
+++ OpenDialogConsoleNetFramework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenDialogConsoleNetFramework
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (DialogResult.OK == dialog.ShowDialog())
+            {
+                string path = dialog.FileName;
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
